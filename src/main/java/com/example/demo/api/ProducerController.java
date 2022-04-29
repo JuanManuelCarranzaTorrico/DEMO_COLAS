691c2d9e20a6ca74065f191fa1e75d82e7f7c490
Juan Manuel Carranza Torrico <ikkicarranza@gmail.com>
2022-04-28 21:35:26 -0400

    Get con conexion al rabbitmq de students

--- src/main/java/com/example/demo/api/ProducerController.java
+++ src/main/java/com/example/demo/api/ProducerController.java
@@ -4,11 +4,16 @@ import com.example.demo.config.RabbitMqConfig;
 
 import com.example.demo.dto.*;
 import com.example.demo.dto.StudentDto;
+import com.example.demo.entity.Student;
+import com.example.demo.bl.StudentBl;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -16,7 +21,8 @@ import java.util.UUID;
  */
 @RestController
 public class ProducerController {
-
+    @Autowired
+    private StudentBl studentBl;
     @Autowired
     private RabbitTemplate template;
 
@@ -45,5 +51,16 @@ public class ProducerController {
     public String sendSubject(@RequestBody SubjectDto subjectDto) {
         template.convertAndSend(RabbitMqConfig.SU_EXCHANGE, RabbitMqConfig.SU_ROUTING_KEY, subjectDto);
         return "Subject Enviado";
+}
+//  Get students
+    @RequestMapping(method = RequestMethod.GET, value = "/v1/api/Student")
+    public ResponseEntity<List<Student>> getStudents() {
+    List<Student> studentList = studentBl.getStudents();
+    for (int i=0; i<studentList.size();i++){
+        template.convertAndSend(RabbitMqConfig.S_EXCHANGE, RabbitMqConfig.S_ROUTING_KEY, studentList.get(i));
+        //System.out.println(studentList.get(i));
+    }
+
+    return new ResponseEntity<>(studentList, HttpStatus.OK);
 }
 }
